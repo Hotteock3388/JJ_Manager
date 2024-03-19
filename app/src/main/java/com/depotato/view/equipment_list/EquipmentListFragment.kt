c8c37d7c1211ac3be3efa167742e4c5790438195
Hotteock <s19046@gsm.hs.kr>
2024-03-19 21:51:31 +0900

    Fragment 이름 변경 (기자재 관리 -> 기자재 목록)

--- app/src/main/java/com/depotato/view/equipment_list/EquipmentListFragment.kt
+++ app/src/main/java/com/depotato/view/equipment_list/EquipmentListFragment.kt
@@ -1,4 +1,4 @@
-package com.depotato.view.manage_equipment
+package com.depotato.view.equipment_list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import com.depotato.jubjub_manager.R
 
 
-class ManageEquipmentFragment : Fragment() {
+class EquipmentListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,7 +17,7 @@ class ManageEquipmentFragment : Fragment() {
         // Inflate the layout for this fragment
 
 
-        return inflater.inflate(R.layout.fragment_manage_equipment, container, false)
+        return inflater.inflate(R.layout.fragment_equipment_list, container, false)
 
     }
 
