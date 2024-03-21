70c1ce8bd2c9b4d6c259adb88f21caebb76fa7c5
Hotteock <s19046@gsm.hs.kr>
2024-03-21 13:53:38 +0900

    기자재 목록 RecyclerView Adapter 생성, 등록

--- app/src/main/java/com/depotato/view/equipment_list/EquipmentListFragment.kt
+++ app/src/main/java/com/depotato/view/equipment_list/EquipmentListFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.depotato.jubjub_manager.R
+import com.depotato.jubjub_manager.databinding.FragmentEquipmentListBinding
+import com.depotato.view.equipment_list.adapter.Equipment
+import com.depotato.view.equipment_list.adapter.EquipmentListRVAdapter
 
 
 class EquipmentListFragment : Fragment() {
@@ -16,9 +20,21 @@ class EquipmentListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
+        val binding = DataBindingUtil.inflate<FragmentEquipmentListBinding>(inflater, R.layout.fragment_equipment_list, container, false)
 
-        return inflater.inflate(R.layout.fragment_equipment_list, container, false)
+        val dataList = arrayListOf(
+            Equipment("애플 iPad Pro 11형", "패드 & 탭", 14, 20, ""),
+            Equipment("Macbook 13인치", "노트북", 2, 10, ""),
+            Equipment("Magic Mouse 2", "마우스", 10, 10, "")
+        )
+        val adapter = EquipmentListRVAdapter(dataList)
 
+        binding.recyclerViewEquipmentList.adapter = adapter
+
+        adapter.notifyDataSetChanged()
+
+
+        return binding.root
     }
 
 }
---
+++ app/src/main/java/com/depotato/view/equipment_list/adapter/EquipmentListRVAdapter.kt
@@ -0,0 +1,40 @@
+package com.depotato.view.equipment_list.adapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.depotato.jubjub_manager.R
+import com.depotato.jubjub_manager.databinding.LayoutEquipmentListItemBinding
+
+class EquipmentListRVAdapter(val dataList: ArrayList<Equipment>): RecyclerView.Adapter<EquipmentListRVAdapter.ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val b = DataBindingUtil.inflate<LayoutEquipmentListItemBinding>(LayoutInflater.from(parent.context), R.layout.layout_equipment_list_item, parent, false)
+        return ViewHolder(b)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, itemPosition: Int) {
+        holder.bind(dataList[itemPosition])
+    }
+
+    class ViewHolder(private val binding : LayoutEquipmentListItemBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Equipment){
+
+            binding.data = data
+
+        }
+    }
+}
+
+data class Equipment(
+    val name: String,
+    val category: String,
+    val currentAmount: Int,
+    val maxAmount: Int,
+    val imageUrl: String
+)
