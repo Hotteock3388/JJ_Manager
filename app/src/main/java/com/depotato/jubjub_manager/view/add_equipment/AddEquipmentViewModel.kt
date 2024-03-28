6ee248de35352100f6d7b673966491cdbc220cb2
Hotteock <s19046@gsm.hs.kr>
2024-03-28 15:24:11 +0900

    AddEuipqmentViewModel 생성

---
+++ app/src/main/java/com/depotato/jubjub_manager/view/add_equipment/AddEquipmentViewModel.kt
@@ -0,0 +1,9 @@
+package com.depotato.jubjub_manager.view.add_equipment
+
+import com.depotato.jubjub_manager.base.BaseViewModel
+
+class AddEquipmentViewModel: BaseViewModel("AddEquipmentViewModel") {
+
+
+
+}
