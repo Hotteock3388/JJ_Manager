802df7d46f9d024260449eb6ce4e390e8626e2fc
Hotteock <s19046@gsm.hs.kr>
2024-04-08 23:53:50 +0900

    기자재 잔여 수량 항목 추가

--- app/src/main/java/com/depotato/jubjub_manager/view/add_equipment/AddEquipmentViewModel.kt
+++ app/src/main/java/com/depotato/jubjub_manager/view/add_equipment/AddEquipmentViewModel.kt
@@ -2,11 +2,9 @@ package com.depotato.jubjub_manager.view.add_equipment
 
 import android.net.Uri
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import androidx.lifecycle.MutableLiveData
 import com.depotato.jubjub_manager.base.BaseViewModel
 import com.depotato.jubjub_manager.function_module.SingleEventLiveData
-import com.depotato.jubjub_manager.view.equipment_list.adapter.Equipment
 import java.io.File
 
 class AddEquipmentViewModel: BaseViewModel("AddEquipmentViewModel") {
@@ -22,7 +20,8 @@ class AddEquipmentViewModel: BaseViewModel("AddEquipmentViewModel") {
     var equipmentImage = File("")
 
     val equipmentName = MutableLiveData<String>("")
-    val equipmentAmount = MutableLiveData<String>("")
+    val equipmentMaxAmount = MutableLiveData<String>("")
+    val equipmentCurrentAmount = MutableLiveData<String>("")
     private var equipmentCategory = ""
 
     val addComplete = SingleEventLiveData<Unit>()
@@ -43,8 +42,8 @@ class AddEquipmentViewModel: BaseViewModel("AddEquipmentViewModel") {
         val equipment = UploadEquipment(
             equipmentName.value!!,
             equipmentCategory,
-            equipmentAmount.value!!.toInt(),
-            equipmentAmount.value!!.toInt(),
+            equipmentMaxAmount.value!!.toInt(),
+            equipmentCurrentAmount.value!!.toInt(),
             equipmentImage
         )
         _toastMessage.value = "$equipment"
@@ -58,17 +57,21 @@ class AddEquipmentViewModel: BaseViewModel("AddEquipmentViewModel") {
             invalidData("기자재 사진을 등록해주세요")
         } else if(equipmentName.value!!.isBlank()){
             invalidData("기자재 이름을 입력해주세요.")
-        } else if(equipmentAmount.value!!.isBlank()){
-            invalidData("기자재 수량을 입력해주세요.")
+        } else if(equipmentMaxAmount.value!!.isBlank()){
+            invalidData("기자재 전체 수량을 입력해주세요.")
+        } else if(equipmentCurrentAmount.value!!.isBlank()){
+            invalidData("기자재 잔여 수량을 입력해주세요.")
         } else if(equipmentCategory.isBlank()){
             invalidData("기자재 카테고리를 선택해주세요.")
+        }  else if(equipmentMaxAmount.value!!.toInt() < equipmentCurrentAmount.value!!.toInt()){
+            invalidData("전체 수량이 잔여 수량보다 많아야 합니다.")
         } else{
             true
         }
 
     }
 
-    fun invalidData(msg: String): Boolean {
+    private fun invalidData(msg: String): Boolean {
         _toastMessage.value = msg
         return false
     }
